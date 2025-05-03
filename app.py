feafeb7484489f53a96b627bf2232e41492ba9a2
Vitu07 <vitormorais20092004@gmail.com>
2025-05-02 21:14:39 -0300

    remove rest endpoint

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 5000))  # Pega a porta do Render
-    uvicorn.run(app, host="0.0.0.0", port=port)  # Escuta na porta correta
+    port = int(os.environ.get("PORT", 5000))  
+    uvicorn.run(app, host="0.0.0.0", port=port)
